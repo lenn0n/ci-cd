6f13950d7055829c19266997ea8715409eaf3890
xennon <gameoveralisa@gmail.com>
2024-02-28 20:25:42 +0800

    Port Express to 5000

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { NextFunction, Request, Response } from "express"
 import routes from "@routes/api-routes"
 import cors from 'cors';
-import path from "path";
+// import path from "path";
 
 require('dotenv').config();
 
@@ -12,12 +12,12 @@ const expressParseOptions = {
 
 app.use(express.json(expressParseOptions));
 app.use(cors());
-app.use(express.static(path.join(__dirname, "frontend", "build")));
 app.use("/api", routes);
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", req.originalUrl));
-});
+// app.use(express.static(path.join(__dirname, "frontend", "build")));
+// app.get("/", (req: Request, res: Response, next: NextFunction) => {
+//   res.sendFile(path.join(__dirname, "frontend", "build", req.originalUrl));
+// });
 
 app.listen(process.env.SERVER_PORT, ()=> {
   console.info(`API Server is now running on port ${process.env.SERVER_PORT}.`)
